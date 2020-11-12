781664c683ffbbd1dc938e83b392611c7c058b2d
Florian Wilisch <florian@wilisch.org>
2020-11-12 16:46:23 +0000

    nav and home

--- frontend/App.js
+++ frontend/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './components/Home'
-import SingleLocation from './components/SingleLocation'
-import Locations from './components/Locations'
-import Register from './components/Register'
-import Login from './components/Login'
+// import SingleLocation from './components/SingleLocation'
+// import Locations from './components/Locations'
+// import Register from './components/Register'
+// import Login from './components/Login'
 import Navbar from './components/Navbar'
-import AddLocation from './components/AddLocation'
-import EditLocation from './components/EditLocation'
-import EditComment from './components/EditComment'
+// import AddLocation from './components/AddLocation'
+// import EditLocation from './components/EditLocation'
+// import EditComment from './components/EditComment'
 
 import './styles/style.scss'
 
@@ -19,23 +19,23 @@ import './styles/style.scss'
 
 
 
-const App = () => {
+const App = () => (
 
-  // <BrowserRouter>
-  //   <Navbar/>
-  //   <Switch>
-  //     <Route exact path='/' component={Home}/>
-  //     <Route exact path='/locations' component={Locations}/>
-  //     <Route exact path='/register' component={Register}/>
-  //     <Route exact path='/login' component={Login}/>
-  //     <Route exact path='/locations/new-location' component={AddLocation}/>
-  //     <Route exact path='/locations/edit-location/:locationId' component={EditLocation}/>
-  //     <Route exact path='/locations/:locationId' component={SingleLocation}/>
-  //     <Route exact path='/locations/edit-comment/:locationId/:commentId/:commentIndex' component={EditComment}/>
-  //   </Switch>
-  // </BrowserRouter>
+  <BrowserRouter>
+    <Navbar/>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      {/* <Route exact path='/locations' component={Locations}/>
+      <Route exact path='/register' component={Register}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/locations/new-location' component={AddLocation}/>
+      <Route exact path='/locations/edit-location/:locationId' component={EditLocation}/>
+      <Route exact path='/locations/:locationId' component={SingleLocation}/>
+      <Route exact path='/locations/edit-comment/:locationId/:commentId/:commentIndex' component={EditComment}/> */}
+    </Switch>
+  </BrowserRouter>
 
-  return <button className="button is-primary" >Hello friends</button>
-}
+  // return <button className="button is-primary" >Hello friends</button>
+)
 
 export default App
---
+++ frontend/components/Home.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+import { Link } from 'react-router-dom'
+// import bulma from 'bulma'
+
+const Home = () => {
+
+  return <section className="hero is-link is-fullheight-with-navbar">
+    <div className="hero-body">
+      <div className="container">
+        <p className="title has-text-centered">
+          GreenWorld
+        </p>
+      </div>
+    </div>
+  </section>
+}
+
+export default Home
