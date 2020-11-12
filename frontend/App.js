cab34531c44162ca9e3cccd9119361c159b02390
Florian Wilisch <florian@wilisch.org>
2020-11-12 13:45:25 +0000

    with seed data

--- frontend/App.js
+++ frontend/App.js
@@ -30,7 +30,7 @@ const App = () => {
       <Route exact path='/locations/new-location' component={AddLocation}/>
       <Route exact path='/locations/edit-location/:locationId' component={EditLocation}/>
       <Route exact path='/locations/:locationId' component={SingleLocation}/>
-      <Route exact path='/locations/edit-comment/:locationId' component={EditComment}/>
+      <Route exact path='/locations/edit-comment/:locationId/:commentId/:commentIndex' component={EditComment}/>
     </Switch>
   </BrowserRouter>
   
