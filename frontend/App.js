89dd6ccd42f64b968f458b931c498d8158d8f20b
James Bolton <copse2209@gmail.com>
2020-11-12 16:57:03 +0000

    LocationList

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Home from './components/Home'
-import SingleLocation from './components/SingleLocation'
+// import Home from './components/Home'
+// import SingleLocation from './components/SingleLocation'
 import Locations from './components/Locations'
-import Register from './components/Register'
-import Login from './components/Login'
-import Navbar from './components/Navbar'
-import AddLocation from './components/AddLocation'
-import EditLocation from './components/EditLocation'
-import EditComment from './components/EditComment'
+// import Register from './components/Register'
+// import Login from './components/Login'
+// import Navbar from './components/Navbar'
+// import AddLocation from './components/AddLocation'
+// import EditLocation from './components/EditLocation'
+// import EditComment from './components/EditComment'
 
 import './styles/style.scss'
 
@@ -19,23 +19,23 @@ import './styles/style.scss'
 
 
 
-const App = () => {
+const App = () => (
 
-  // <BrowserRouter>
-  //   <Navbar/>
-  //   <Switch>
-  //     <Route exact path='/' component={Home}/>
-  //     <Route exact path='/locations' component={Locations}/>
-  //     <Route exact path='/register' component={Register}/>
+  <BrowserRouter>
+    {/* <Navbar/> */}
+    <Switch>
+ {/*     <Route exact path='/' component={Home}/> */}
+      <Route exact path='/locations' component={Locations}/>
+      {/*     <Route exact path='/register' component={Register}/>
   //     <Route exact path='/login' component={Login}/>
   //     <Route exact path='/locations/new-location' component={AddLocation}/>
   //     <Route exact path='/locations/edit-location/:locationId' component={EditLocation}/>
   //     <Route exact path='/locations/:locationId' component={SingleLocation}/>
-  //     <Route exact path='/locations/edit-comment/:locationId/:commentId/:commentIndex' component={EditComment}/>
-  //   </Switch>
-  // </BrowserRouter>
+  //     <Route exact path='/locations/edit-comment/:locationId/:commentId/:commentIndex' component={EditComment}/> */}
+    </Switch>
+  </BrowserRouter>
 
-  return <button className="button is-primary" >Hello friends</button>
-}
+  // return <button className="button is-primary" >Hello friends</button>
+)
 
 export default App
